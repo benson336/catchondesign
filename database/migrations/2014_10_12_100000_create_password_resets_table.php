7b467b5acf2f24af36a2f3bc4f9bf23e5eeb82b3
unknown <benson.wen36@gmail.com>
2021-07-14 12:12:28 -0700

    7_14_commit5

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+DB::statement('SET SESSION sql_require_primary_key=0');
 
 class CreatePasswordResetsTable extends Migration
 {
